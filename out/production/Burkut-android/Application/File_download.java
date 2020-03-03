0ee4cdb3be7a9249ba09b40bedb67395d475b153
Burkut60 <=kamilla2016@111>
2020-03-03 22:07:24 +0300

    fixed version of homework 3

--- out/production/Burkut-android/Application/File_download.java
+++ out/production/Burkut-android/Application/File_download.java
@@ -1,6 +1,9 @@
 package Application;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 
@@ -10,4 +13,31 @@ public class File_download {
         IntPut.getPut(newUrl);
 
     }
+}
+class IntPut  {
+
+    public static void getPut(URL b) throws IOException {
+
+        String newFile = "newFile.txt";
+        URL myUrl =  b;
+
+        InputStream input = new BufferedInputStream(myUrl.openStream());
+        FileOutputStream outPut = new FileOutputStream(newFile);
+
+        System.out.print("Download start!");
+
+        byte buffer [] =new byte[1024];
+        while (input.read(buffer) !=-1){
+            outPut.write(buffer);
+        }
+        System.out.print(" Download finish! ");
+
+        input.close();
+        outPut.close();
+
+
+    }
+}
+class Parser{
+
 }
